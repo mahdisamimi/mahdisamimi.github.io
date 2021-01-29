571af2e82bcbdd469f460db4826c1c6052fe41d8
Mahdi Samimi <mahdisamimiiut@gmail.com>
2021-01-29 13:11:46 +0330

    Update nav-bar.js

--- assets/js/nav-bar.js
+++ assets/js/nav-bar.js
@@ -1,1 +1,8 @@
+window.onscroll = function() {myFunction()};
 
+        function myFunction() {
+          var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+          var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+          var scrolled = (winScroll / height) * 100;
+          document.getElementById("scroll-progress-bar").style.width = scrolled + "%";
+        }
