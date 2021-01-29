3aa3105d25f35435809b83381cb5769bec70de25
Mahdi Samimi <mahdisamimiiut@gmail.com>
2021-01-29 14:24:24 +0330

    Update nav-bar.js

--- assets/js/nav-bar.js
+++ assets/js/nav-bar.js
@@ -4,8 +4,8 @@ window.onscroll = function() {myFunction()};
           var headerHeight = document.getElementsByTagName("HEADER")[0].clientHeight;
           var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
           var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-          var scrolled = (winScroll-headerHeight / height-headerHeight) * 100;
           if (winScroll >= headerHeight){
+                var scrolled = (winScroll-headerHeight / height-headerHeight) * 100;
                 document.getElementById("scroll-progress-bar").style.width = scrolled + "%";
           }
         }
