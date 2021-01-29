128f5900afb4f46475d1412a753b7e0ce846dfea
Mahdi Samimi <mahdisamimiiut@gmail.com>
2021-01-29 03:34:14 +0330

    Update nav-bar.js

--- assets/js/nav-bar.js
+++ assets/js/nav-bar.js
@@ -1,10 +1,1 @@
 
-$(window).scroll(function() {
-    var stickyNavTop = $('ul').offset().top;
-    if ($(window).scrollTop() > stickyNavTop) {
-        $('ul').addClass('fixed');
-    }
-    else {
-        $('ul').removeClass('fixed');
-    }  
-});
